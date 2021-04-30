f611a8ad0543def9c32bb9fe1e92df0df8549da9
Noah Nam <noah@apartalk.com>
2021-05-01 00:51:15 +0900

    widow test code 삭제

--- tests/core/domains/user/repository/test_user_repository.py
+++ tests/core/domains/user/repository/test_user_repository.py
@@ -1,38 +1,7 @@
 from app.persistence.model.user_model import UserModel
-from core.domains.user.repository.user_repository import UserRepository
 
 
-def test_create_user_when_not_use_factory_boy(session):
-    user = UserModel(nickname="Noah")
-    session.add(user)
-    session.commit()
-
-    result = session.query(UserModel).first()
-
-    assert result.nickname == user.nickname
-
-
-def test_get_user(session):
-    user = UserModel(nickname="Tester")
-    session.add(user)
-    session.commit()
-
-    user_entity = UserRepository().get_user(user_id=user.id)
-
-    assert user_entity == user.to_entity()
-
-
-def test_create_user_when_use_create_users_fixture_then_make_two_users(session, create_users):
-    users = session.query(UserModel).all()
-
-    assert len(users) == 2
-    assert users[0].nickname == "test_user_0"
-
-
-def test_compare_create_user_when_use_build_batch_and_create_users_fixture(session, create_users, normal_user_factory):
-    fixture_users = session.query(UserModel).all()
-    build_batch_users = normal_user_factory.build_batch(size=3, nickname="Noah", sex="F")
-
-    assert len(fixture_users) == 2
-    assert len(build_batch_users) == 3
-    assert fixture_users[0].sex != build_batch_users[0].sex == 'F'
+def test_create_user_profiles_when_first_login_then_success(session, create_user):
+    users = create_user
+    user = session.query(UserModel).filter_by(id=1).first()
+    assert 1 == 1
