043857d4cf2e43389e8c0bb239d549800a1dd077
Noah Nam <noah@apartalk.com>
2021-05-01 00:49:07 +0900

    CreateUserDto 생성

--- core/domains/user/dto/user_dto.py
+++ core/domains/user/dto/user_dto.py
@@ -3,3 +3,14 @@ from pydantic import BaseModel
 
 class GetUserDto(BaseModel):
     user_id: int = None
+
+
+class CreateUserDto(BaseModel):
+    user_id: int
+    nickname: str
+    email: str = None
+    birthday: str = None
+    gender: str
+    is_active: bool
+    is_out: bool
+    profile_img_id: int = None
