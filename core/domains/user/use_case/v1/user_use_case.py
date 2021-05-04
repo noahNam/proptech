ebde5a198304e4889809ac7d18b5360cfecf5b1c
Noah Nam <noah@apartalk.com>
2021-05-04 13:51:42 +0900

    widow view, test code 제거

--- core/domains/user/use_case/v1/user_use_case.py
+++ core/domains/user/use_case/v1/user_use_case.py
@@ -1,22 +1,9 @@
-from typing import Union, List
-
 import inject
 
-from app.extensions.utils.event_observer import get_event_object, send_message
-from core.domains.user.dto.user_dto import GetUserDto
 from core.domains.user.repository.user_repository import UserRepository
-from core.use_case_output import UseCaseSuccessOutput, UseCaseFailureOutput, FailureType
 
 
-class GetUserUseCase:
+class UserBaseUseCase:
     @inject.autoparams()
     def __init__(self, user_repo: UserRepository):
         self.__user_repo = user_repo
-
-    def execute(
-            self, dto: GetUserDto
-    ) -> Union[UseCaseSuccessOutput, UseCaseFailureOutput]:
-        user = self.__user_repo.get_user(user_id=dto.user_id)
-        if not user:
-            return UseCaseFailureOutput(type=FailureType.NOT_FOUND_ERROR)
-        return UseCaseSuccessOutput(value=user)
