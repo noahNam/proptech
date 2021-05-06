09352e170c760a20e0e190137f01c0dd2e9535de
Noah Nam <noah@apartalk.com>
2021-05-06 18:10:28 +0900

    주석처리 테스트코드 삭제

--- tests/app/http/requests/view/user/v1/test_user_request.py
+++
@@ -1,7 +0,0 @@
-# from app.http.requests.view.user.v1.user_request import GetUserRequest
-#
-#
-# def test_when_invalid_request_then_fail():
-#     result = GetUserRequest(user_id="invalid user id").validate_request_and_make_dto()
-#
-#     assert result is False
