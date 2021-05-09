3adf4b72bc173dfb4757caeb742ff927f7420bf5
Noah Nam <noah@apartalk.com>
2021-05-09 16:50:54 +0900

    swagger 오타,format 수정

--- app/http/view/user/v1/user_view.py
+++ app/http/view/user/v1/user_view.py
@@ -3,17 +3,15 @@ from flask import request
 from flask_jwt_extended import jwt_required
 
 from app.http.requests.v1.user_request import CreateUserSchemeRequest
-from app.http.responses import failure_response
 from app.http.responses.presenters.v1.user_presenter import UserPresenter
 from app.http.view import auth_required, api
 from core.domains.user.use_case.v1.user_use_case import CreateUserUseCase
-from core.use_case_output import UseCaseFailureOutput, FailureType
 
 
 @api.route("/v1/users", methods=["POST"])
 @jwt_required
 @auth_required
-@swag_from("create_uesr.yml", methods=["POST"])
+@swag_from("create_user.yml", methods=["POST"])
 def create_user_view():
     dto = CreateUserSchemeRequest(
         **request.form.to_dict(), file=request.files.getlist("files"),
