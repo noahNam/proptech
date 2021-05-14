2679b2aedbfb5bb8abddcb868757d5a7e4ce2ec2
noah <noah@apartalk.com>
2021-05-15 00:21:43 +0900

    health check 추가

--- app/http/view/main/__init__.py
+++ app/http/view/main/__init__.py
@@ -1,12 +1,9 @@
 from flask import jsonify
-from flask_jwt_extended import jwt_required
 
 from app.__meta__ import __api_name__, __version__
-from app.http.view import auth_required, api
+from app.http.view import api
 
 
-@api.route("/main", methods=["GET"])
-@jwt_required
-@auth_required
+@api.route("/main/heath_check", methods=["GET"])
 def index():
     return jsonify({"name": __api_name__, "version": __version__})
