e15d2191daa8d3a0a5011a7f8a94eb7cc44c33f5
noah <noah@apartalk.com>
2021-05-15 00:53:16 +0900

    오타 수정 heath_check -> health_check

--- app/http/view/main/__init__.py
+++ app/http/view/main/__init__.py
@@ -1,9 +1,9 @@
 from flask import jsonify
 
-from app.__meta__ import __api_name__, __version__
+from app.__meta__ import __version__
 from app.http.view import api
 
 
-@api.route("/main/heath_check", methods=["GET"])
+@api.route("/main/health_check", methods=["GET"])
 def index():
-    return jsonify({"name": __api_name__, "version": __version__})
+    return jsonify({"version": __version__})
