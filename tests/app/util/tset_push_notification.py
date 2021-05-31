a8ea2a3b736c0aed2de8249f020eab72aef745c8
noah <noah@apartalk.com>
2021-05-31 18:14:26 +0900

    add test code -> push notification via lambda (삭제 예정:Hawkeye local_test.py로 대체될 예정)

--- tests/app/util/tset_push_notification.py
+++ tests/app/util/tset_push_notification.py
@@ -1,7 +1,9 @@
 import json
 import logging
+import time
 
 import boto3
+import pytest
 from botocore.exceptions import ClientError
 
 from app.extensions.utils.message_converter import MessageConverter
@@ -26,6 +28,7 @@ message_dto = PushMessageDto(
 )
 
 
+@pytest.mark.skip(reason="삭제 예정:Hawkeye local_test.py로 대체될 예정")
 def test_push_message_via_SNS(
         session
 ):
@@ -35,6 +38,7 @@ def test_push_message_via_SNS(
     # notification message insert
     notification = NotificationModel(
         user_id=1,
+        token="user-token",
         endpoint=ENDPOINT,
         category="APT01",
         data=message_dict,
@@ -50,14 +54,14 @@ def test_push_message_via_SNS(
     # set user_device_token
     user_device_token = ENDPOINT
 
-    # 엔드포인트에 연결할 임의의 사용자 데이터. Amazon SNS는 이 데이터를 사용하지 않습니다. 이 데이터는 UTF-8 형식이어야 하며 2KB 미만이어야 합니다.
+    # 엔드포인트에 연결할 임의의 사용자 데이터. Amazon SNS는 이 데이터를 사용하지 않습니다. 이 데이터는 UTF-8 형식이어야 하며 2KB 미만이어야 한다.
     sns = boto3.resource('sns')
     platform_application = sns.PlatformApplication(APPLICATION_ARN)
 
     try:
         platform_application_endpoint = platform_application.create_platform_endpoint(
             Token=user_device_token,
-            # CustomUserData=data1
+            CustomUserData="1"
         ).arn
     except ClientError as e:
         logger.exception("Could not create to platform application endpoint. %s", e)
@@ -71,3 +75,56 @@ def test_push_message_via_SNS(
     res = topic.publish(Message=message, MessageStructure='json')
 
     assert res.get("ResponseMetadata").get("HTTPStatusCode") == 200
+
+
+@pytest.mark.skip(reason="삭제 예정:Hawkeye local_test.py로 대체될 예정")
+def test_sns_get_count_application_endpoint():
+    start_time = time.time()
+    sns = boto3.client("sns")
+
+    # First call does not need a NextToken
+    resp = sns.list_endpoints_by_platform_application(PlatformApplicationArn=APPLICATION_ARN)
+    count = len(resp['Endpoints'])
+    enabled_count = 0
+    try:
+        while True:
+            for endpoint in resp['Endpoints']:
+                if endpoint['Attributes']['Enabled'] == "true":
+                    enabled_count += 1
+
+            # For the Last request, "NextToken" will not be included,
+            # which will throw an exception. We use this to exit the loop.
+            resp = sns.list_endpoints_by_platform_application(
+                PlatformApplicationArn=APPLICATION_ARN,
+                NextToken=ENDPOINT)
+            count += len(resp['Endpoints'])
+    except Exception as e:
+        pass
+
+    finally:
+        print("Total Number of Endpoints counted: " + str(count))
+        print("Total Number of Endpoints enabled: " + str(enabled_count))
+        print("Percentage of Endpoints enabled: " + str((enabled_count / count) * 100) + "%")
+        print("Query time: %s seconds" % (time.time() - start_time))
+
+
+@pytest.mark.skip(reason="삭제 예정:Hawkeye local_test.py로 대체될 예정")
+def test_sns_get_value_application_endpoint():
+    sns = boto3.client("sns")
+
+    # First call does not need a NextToken
+    resp = sns.list_endpoints_by_platform_application(PlatformApplicationArn=APPLICATION_ARN)
+
+    try:
+        for endpoint in resp['Endpoints']:
+            if endpoint['Attributes']['Enabled'] == "true":
+                endpoint_arn = endpoint['EndpointArn']
+                token = endpoint['Attributes']['Token']
+                custom_user_data = endpoint['Attributes']['CustomUserData']
+    except Exception as e:
+        pass
+
+    finally:
+        print("endpoint_arn : ", endpoint_arn)
+        print("endpoint_arn : ", token)
+        print("endpoint_arn : ", custom_user_data)
