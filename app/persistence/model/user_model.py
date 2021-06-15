4ca1cafc9e0cfd7c10e1d290a9a459002a65e9b8
noah <noah@apartalk.com>
2021-06-15 11:37:59 +0900

    modify user schema model

--- app/persistence/model/user_model.py
+++ app/persistence/model/user_model.py
@@ -10,7 +10,6 @@ from sqlalchemy.orm import relationship, backref
 
 from app import db
 from app.extensions.utils.time_helper import get_server_timestamp
-from core.domains.user.enum.user_enum import UserDefaultValueEnum
 
 
 class UserModel(db.Model):
@@ -19,15 +18,12 @@ class UserModel(db.Model):
     id = Column(
         BigInteger().with_variant(Integer, "sqlite"), primary_key=True, nullable=False
     )
-    nickname = Column(
-        String(20), nullable=False, default=UserDefaultValueEnum.NICKNAME.value
+    is_home_owner = Column(Boolean, nullable=True)
+    interested_house_type = Column(
+        String(10), nullable=True
     )
-    email = Column(String(40), nullable=False)
-    birthday = Column(String(8), nullable=True)
-    gender = Column(String(1), nullable=False)
     is_active = Column(Boolean, nullable=False, default=True)
     is_out = Column(Boolean, nullable=False, default=False)
-    profile_img_id = Column(BigInteger, nullable=True)
     created_at = Column(DateTime, default=get_server_timestamp())
     updated_at = Column(DateTime, default=get_server_timestamp())
 
