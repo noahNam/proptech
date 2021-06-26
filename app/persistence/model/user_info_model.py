7bc9ef0fa3f2ae8443996d4c898c3b5bffe8e799
noah <noah@apartalk.com>
2021-06-26 19:53:02 +0900

    add schema model -> user_info_model, user_profile_model

---
+++ app/persistence/model/user_info_model.py
@@ -0,0 +1,23 @@
+from sqlalchemy import (
+    Column,
+    BigInteger,
+    DateTime,
+    Integer, ForeignKey, String, SmallInteger,
+)
+
+from app import db
+from app.extensions.utils.time_helper import get_server_timestamp
+from app.persistence.model.user_profile_model import UserProfileModel
+
+
+class UserProfileModel(db.Model):
+    __tablename__ = "user_infos"
+
+    id = Column(
+        BigInteger().with_variant(Integer, "sqlite"), primary_key=True, nullable=False
+    )
+    user_profile_id = Column(BigInteger, ForeignKey(UserProfileModel.id), nullable=False)
+    code = Column(SmallInteger, nullable=True)
+    value = Column(String(8), nullable=True)
+    created_at = Column(DateTime, default=get_server_timestamp(), nullable=False)
+    updated_at = Column(DateTime, default=get_server_timestamp(), nullable=False)
---
+++ app/persistence/model/user_profile_model.py
@@ -0,0 +1,27 @@
+from sqlalchemy import (
+    Column,
+    BigInteger,
+    Boolean,
+    DateTime,
+    Integer, ForeignKey, String,
+)
+from sqlalchemy.orm import relationship, backref
+
+from app import db
+from app.extensions.utils.time_helper import get_server_timestamp
+from app.persistence.model import UserModel
+
+
+class UserProfileModel(db.Model):
+    __tablename__ = "user_profiles"
+
+    id = Column(
+        BigInteger().with_variant(Integer, "sqlite"), primary_key=True, nullable=False
+    )
+    user_id = Column(BigInteger, ForeignKey(UserModel.id), nullable=False, unique=True)
+    nickname = Column(String(12), nullable=True)
+    last_update_code = Column(Boolean, nullable=True)
+    created_at = Column(DateTime, default=get_server_timestamp(), nullable=False)
+    updated_at = Column(DateTime, default=get_server_timestamp(), nullable=False)
+
+    user_profiles = relationship("UserProfileModel", backref=backref("user_profiles"))
