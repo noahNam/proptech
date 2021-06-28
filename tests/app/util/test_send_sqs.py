7eae5b5441baee278a42abbecc911b7d3fd32ab4
noah <noah@apartalk.com>
2021-06-28 23:19:01 +0900

    sqs test code 추가

--- tests/app/util/test_send_sqs.py
+++ tests/app/util/test_send_sqs.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 
 from app.extensions.queue import SqsTypeEnum, SenderDto
 from app.extensions.queue.sqs_sender import SqsMessageSender
@@ -13,9 +14,46 @@ def test_send_sqs(sqs: SqsMessageSender):
         msg=dict(user_id=1),
         msg_created_at=get_server_timestamp().strftime("%Y/%m/%d, %H:%M:%S"),
     )
-
-    sqs.send_message(
-        queue_type=SqsTypeEnum.USER_DATA_SYNC_TO_LAKE, msg=msg, logging=True
-    )
+    total = 0
+    while True:
+        sqs.send_message(
+            queue_type=SqsTypeEnum.USER_DATA_SYNC_TO_LAKE, msg=msg, logging=True
+        )
+        total += 1
+        if total == 2:
+            break
 
     assert 1 == 1
+
+
+@pytest.mark.skip(reason="실제 SQS Message 조회")
+def test_receive_sqs(sqs: SqsMessageSender):
+    total = 0
+    try:
+        for message in sqs.receive_after_delete(queue_type=SqsTypeEnum.USER_DATA_SYNC_TO_LAKE):
+            test = message
+            if message['Body']:
+                #### 처리 로직 -> data push to data lake ####
+
+                ##########################################
+                print("[receive_after_delete] Target Message {0}", message['Body'])
+                total += 1
+    except Exception as e:
+        print("SQS Fail : {}".format(e))
+        send_slack_message('Exception: {}'.format(str(e)), "[FAIL]" + SqsTypeEnum.USER_DATA_SYNC_TO_LAKE.value)
+
+    dict_ = {
+        'result': True, 'total': total,
+    }
+    return dict_
+
+
+def send_slack_message(message, title):
+    channel = "#engineering-class"
+
+    text = title + " -> " + message
+    slack_token = "xoxb-1811487173312-2075478573287-JtTJbgy3fAboqlwkLeas9R1o"
+    requests.post("https://slack.com/api/chat.postMessage",
+                  headers={"Authorization": "Bearer " + slack_token},
+                  data={"channel": channel, "text": text}
+                  )
