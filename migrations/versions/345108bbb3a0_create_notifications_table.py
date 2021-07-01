0c1b30ef7a3887b658a8a40c8772c6ad44bd308b
noah <noah@apartalk.com>
2021-07-01 23:27:43 +0900

    notifications index 추가

--- migrations/versions/345108bbb3a0_create_notifications_table.py
+++ migrations/versions/345108bbb3a0_create_notifications_table.py
@@ -1,8 +1,8 @@
 """create notifications table
 
-Revision ID: 407554a1fdaf
-Revises: adb1abcd2216
-Create Date: 2021-06-28 16:10:29.516221
+Revision ID: 345108bbb3a0
+Revises: a7e863bcd3be
+Create Date: 2021-07-01 23:24:54.062512
 
 """
 from alembic import op
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = '407554a1fdaf'
-down_revision = 'adb1abcd2216'
+revision = '345108bbb3a0'
+down_revision = 'a7e863bcd3be'
 branch_labels = None
 depends_on = None
 
@@ -24,7 +24,7 @@ def upgrade():
                     sa.Column('endpoint', sa.String(length=100), nullable=True),
                     sa.Column('topic', sa.String(length=6), nullable=False),
                     sa.Column('badge_type', sa.String(length=3), nullable=False),
-                    sa.Column('message', postgresql.JSONB(astext_type=sa.Text()).with_variant(sa.JSON(), 'sqlite'),
+                    sa.Column('data', postgresql.JSONB(astext_type=sa.Text()).with_variant(sa.JSON(), 'sqlite'),
                               nullable=False),
                     sa.Column('is_read', sa.Boolean(), nullable=False),
                     sa.Column('is_pending', sa.Boolean(), nullable=False),
@@ -33,7 +33,13 @@ def upgrade():
                     sa.Column('updated_at', sa.DateTime(), nullable=False),
                     sa.PrimaryKeyConstraint('id')
                     )
+    op.create_index(op.f('ix_notifications_status'), 'notifications', ['status'], unique=False)
+    op.create_index(op.f('ix_notifications_topic'), 'notifications', ['topic'], unique=False)
+    op.create_index(op.f('ix_notifications_user_id'), 'notifications', ['user_id'], unique=False)
 
 
 def downgrade():
+    op.drop_index(op.f('ix_notifications_user_id'), table_name='notifications')
+    op.drop_index(op.f('ix_notifications_topic'), table_name='notifications')
+    op.drop_index(op.f('ix_notifications_status'), table_name='notifications')
     op.drop_table('notifications')
