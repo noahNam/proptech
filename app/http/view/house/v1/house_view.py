d36218c67500ee94ebb9008e49884866ac731a7f
noah <noah@apartalk.com>
2021-07-06 16:19:51 +0900

    apidocs 추가

--- app/http/view/house/v1/house_view.py
+++ app/http/view/house/v1/house_view.py
@@ -10,7 +10,7 @@ from core.domains.house.use_case.v1.house_use_case import UpsertInterestHouseUse
 @api.route("/v1/houses/<int:house_id>/like", methods=["POST"])
 @jwt_required
 @auth_required
-@swag_from("update_notification.yml", methods=["POST"])
+@swag_from("upsert_interest_house.yml", methods=["POST"])
 def upsert_interest_house_view(house_id):
     dto = UpsertInterestHouseRequestSchema(
         house_id=house_id, user_id=current_user.id, **request.get_json()
