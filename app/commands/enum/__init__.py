faf10b3b685d87059190729bee4f91f3f64b181f
noah <noah@apartalk.com>
2021-07-07 01:00:19 +0900

    walking-skeleton for cron-walker

---
+++ app/commands/enum/__init__.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+
+class TopicEnum(Enum):
+    PRE_PROCESSING_INTEREST_HOUSE_DATA = "tanos.pre_processing.interest_house_data.v1"
--- app/commands/worker.py
+++ app/commands/worker.py
@@ -1,8 +1,17 @@
+import click
 from flask import current_app
+
+from app.commands.enum import TopicEnum
+from core.domains.house.use_case.v1.house_use_case import PrePrcsInterestHouseUseCase
+
+
+def get_worker(topic: str):
+    if topic == TopicEnum.PRE_PRCS_INTEREST_HOUSE.value:
+        return PrePrcsInterestHouseUseCase(topic=topic)
 
 
 @current_app.cli.command("start-worker")
-def start_worker():
-    print("*" * 30)
-    print("*" * 30)
-    print("*" * 30)
+@click.argument("topic")
+def start_worker(topic):
+    us = get_worker(topic)
+    us.execute()
