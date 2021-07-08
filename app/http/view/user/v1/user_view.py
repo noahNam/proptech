0f88731e1898276f1a61577360d6fdc0a557a21d
noah <noah@apartalk.com>
2021-07-08 22:45:17 +0900

    user provider search call api 구현

--- app/http/view/user/v1/user_view.py
+++ app/http/view/user/v1/user_view.py
@@ -1,5 +1,8 @@
+from http import HTTPStatus
+
+import requests
 from flasgger import swag_from
-from flask import request
+from flask import request, jsonify
 
 from app.http.requests.v1.user_request import (
     CreateUserRequestSchema,
@@ -14,6 +17,7 @@ from app.http.responses.presenters.v1.user_presenter import (
     GetUserInfoPresenter, GetUserPresenter,
 )
 from app.http.view import auth_required, api, current_user, jwt_required
+from core.domains.user.enum.user_enum import UserProviderCallEnum
 from core.domains.user.use_case.v1.user_use_case import (
     CreateUserUseCase,
     CreateAppAgreeTermsUseCase,
@@ -82,3 +86,26 @@ def get_user_info_view():
     ).validate_request_and_make_dto()
 
     return GetUserInfoPresenter().transform(GetUserInfoUseCase().execute(dto=dto))
+
+
+@api.route("/v1/users/provider", methods=["GET"])
+@jwt_required
+@auth_required
+@swag_from("get_user_info.yml", methods=["GET"])
+def get_user_provider_view():
+    auth_header = request.headers.get("Authorization")
+
+    try:
+        response = requests.get(
+            url=UserProviderCallEnum.CAPTAIN_BASE_URL.value + UserProviderCallEnum.CALL_END_POINT.value,
+            headers={
+                "Content-Type": "application/json",
+                "Cache-Control": "no-cache",
+                "Authorization": auth_header
+            },
+        )
+    except Exception:
+        raise Exception
+
+    data = response.json()
+    return jsonify(data), HTTPStatus.OK
