a73b19deff88bf59d69db45c0ccab6a7ff1a2fc8
noah <noah@apartalk.com>
2021-07-14 01:01:15 +0900

    worker workflow 구현

--- app/commands/worker.py
+++ app/commands/worker.py
@@ -2,12 +2,12 @@ import click
 from flask import current_app
 
 from app.commands.enum import TopicEnum
-from core.domains.house.use_case.v1.house_use_case import PrePrcsInterestHouseUseCase
+from core.domains.notification.use_case.v1.notification_worker_use_case import PrePrcsNotificationUseCase
 
 
 def get_worker(topic: str):
     if topic == TopicEnum.PRE_PRCS_INTEREST_HOUSE.value:
-        return PrePrcsInterestHouseUseCase(topic=topic)
+        return PrePrcsNotificationUseCase(topic=topic)
 
 
 @current_app.cli.command("start-worker")
@@ -15,3 +15,7 @@ def get_worker(topic: str):
 def start_worker(topic):
     us = get_worker(topic)
     us.execute()
+
+
+if __name__ == "__main__":
+    start_worker(topic="tanos.pre_prcs_interest_house_data.v1")
