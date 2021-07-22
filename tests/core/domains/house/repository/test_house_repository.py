2ab781a91163a6865aae971c0a89fd528ce90550
noah <noah@apartalk.com>
2021-07-22 20:14:53 +0900

    관심록록 조회 walking skeleton

--- tests/core/domains/house/repository/test_house_repository.py
+++ tests/core/domains/house/repository/test_house_repository.py
@@ -46,3 +46,13 @@ def test_update_is_like_house_repo_when_unlike_public_sales_then_success(session
     assert result.user_id == upsert_interest_house_dto.user_id
     assert result.house_id == upsert_interest_house_dto.house_id
     assert result.type == upsert_interest_house_dto.type
+
+
+def test_get_interest_house_list(session, interest_house_factory):
+    interest_house = interest_house_factory.build()
+    session.add(interest_house)
+    session.commit()
+
+    HouseRepository().get_interest_house_list(user_id=1)
+
+    assert 1 == 1
