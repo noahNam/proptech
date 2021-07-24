16f270695a3b141719670eb0457fa7feb8f43d4a
Harry <harry@apartalk.com>
2021-07-25 02:07:25 +0900

    test_rds config 삭제

--- app/config.py
+++ app/config.py
@@ -41,9 +41,6 @@ class LocalConfig(Config):
     SQLALCHEMY_ECHO = True
     DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:skarlgur117@test-tanos-rds" \
-                              ".cluster-cmib56uiilha.ap-northeast-2.rds.amazonaws.com:5432/tanos"
-
     # Local environment configuration using Docker API service
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:1234@postgres:5432/tanos"
     # Prod migrate
