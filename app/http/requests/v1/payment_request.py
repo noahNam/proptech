141a88cf47ecbcabcfd54992d2187260466cb5c1
noah <noah@apartalk.com>
2021-08-02 16:16:19 +0900

    api 이름 변경: use_ticket -> use_basic_ticket

--- app/http/requests/v1/payment_request.py
+++ app/http/requests/v1/payment_request.py
@@ -5,7 +5,7 @@ from pydantic import (
 )
 
 from app.extensions.utils.log_helper import logger_
-from core.domains.user.dto.user_dto import GetUserDto
+from core.domains.payment.dto.payment_dto import PaymentUserDto, UseTicketDto
 from core.exceptions import InvalidRequestException
 
 logger = logger_.getLogger(__name__)
@@ -15,16 +15,37 @@ class GetTicketUsageResultSchema(BaseModel):
     user_id: StrictInt
 
 
+class UseTicketSchema(BaseModel):
+    user_id: StrictInt
+    house_id: StrictInt
+
+
 class GetTicketUsageResultRequestSchema:
     def __init__(self, user_id):
         self.user_id = int(user_id) if user_id else None
 
     def validate_request_and_make_dto(self):
         try:
-            schema = GetTicketUsageResultSchema(user_id=self.user_id,).dict()
-            return GetUserDto(**schema)
+            schema = GetTicketUsageResultSchema(user_id=self.user_id, ).dict()
+            return PaymentUserDto(**schema)
         except ValidationError as e:
             logger.error(
                 f"[GetTicketUsageResultRequestSchema][validate_request_and_make_dto] error : {e}"
+            )
+            raise InvalidRequestException(message=e.errors())
+
+
+class UseBasicTicketRequestSchema:
+    def __init__(self, user_id, house_id):
+        self.user_id = int(user_id) if user_id else None
+        self.house_id = house_id
+
+    def validate_request_and_make_dto(self):
+        try:
+            schema = UseTicketSchema(user_id=self.user_id, house_id=self.house_id).dict()
+            return UseTicketDto(**schema)
+        except ValidationError as e:
+            logger.error(
+                f"[UseTicketRequestSchema][validate_request_and_make_dto] error : {e}"
             )
             raise InvalidRequestException(message=e.errors())
