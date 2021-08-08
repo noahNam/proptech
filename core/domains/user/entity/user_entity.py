a7bd82280875503ba58c0d7a32fcc758002d8d38
noah <noah@apartalk.com>
2021-08-08 16:09:18 +0900

    회원가입 시 email, phone_number 추가 로직 구현

--- core/domains/user/entity/user_entity.py
+++ core/domains/user/entity/user_entity.py
@@ -87,7 +87,7 @@ class UserProfileEntity(BaseModel):
 
 class UserEntity(BaseModel):
     id: int
-    email: str
+    email: Optional[str]
     is_required_agree_terms: bool
     join_date: str
     is_active: bool
