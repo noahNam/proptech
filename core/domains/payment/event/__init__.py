ad0b988c3972af9f080adf9291c38189864f7dce
noah <noah@apartalk.com>
2021-08-17 23:00:51 +0900

    설문완료시 티켓 추가 : 테스트 코드 추가

--- core/domains/payment/event/__init__.py
+++ core/domains/payment/event/__init__.py
@@ -6,8 +6,9 @@ from core.domains.payment.repository.payment_repository import PaymentRepository
 
 
 def create_join_ticket(user_id: int,):
-    if not PaymentRepository.is_join_ticket(user_id=user_id):
-        PaymentRepository.create_join_ticket(user_id=user_id)
+    test = PaymentRepository().is_join_ticket(user_id=user_id)
+    if not PaymentRepository().is_join_ticket(user_id=user_id):
+        PaymentRepository().create_join_ticket(user_id=user_id)
 
     setattr(g, PaymentTopicEnum.CREATE_JOIN_TICKET, None)
 
