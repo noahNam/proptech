7a5ba955d0c1dae1fb872135807aed9fdaea83df
noah <noah@apartalk.com>
2021-08-19 11:49:24 +0900

    slack token 환경변수로 변경

--- core/domains/notification/use_case/v1/notification_worker_use_case.py
+++ core/domains/notification/use_case/v1/notification_worker_use_case.py
@@ -35,7 +35,7 @@ class PrePrcsNotificationUseCase:
         channel = "#engineering-class"
 
         text = "[Batch Error] PrePrcsNotificationUseCase -> " + message
-        slack_token = "***REMOVED***"
+        slack_token = os.environ.get("SLACK_TOKEN")
         requests.post(
             "https://slack.com/api/chat.postMessage",
             headers={"Authorization": "Bearer " + slack_token},
