b2738f0566f9d7b960da19f8d220d4ef2ced4877
Harry <harry@apartalk.com>
2021-08-23 16:01:08 +0900

    feat: special, general supply_result 테이블 추가

---
+++ app/persistence/model/general_supply_result_model.py
@@ -0,0 +1,29 @@
+from sqlalchemy import (
+    Column,
+    BigInteger,
+    Integer,
+    ForeignKey,
+    String,
+    SmallInteger,
+)
+
+from app import db
+from app.persistence.model.public_sale_detail_model import PublicSaleDetailModel
+
+
+class GeneralSupplyResultModel(db.Model):
+    __tablename__ = "general_supply_results"
+
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
+    public_sale_details_id = Column(
+        BigInteger,
+        ForeignKey(PublicSaleDetailModel.id, ondelete="CASCADE"),
+        nullable=False,
+    )
+
+    region = Column(String(10), nullable=True)
+    region_percent = Column(SmallInteger, nullable=True)
+    multi_children_vol = Column(SmallInteger, nullable=True)
+    applicant_num = Column(SmallInteger, nullable=True)
+    competition_rate = Column(SmallInteger, nullable=True)
+    win_point = Column(SmallInteger, nullable=True)
