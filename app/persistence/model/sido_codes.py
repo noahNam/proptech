6cf1c83963d95384581c1033f8e4140790bb2b46
noah <noah@apartalk.com>
2021-08-31 00:00:12 +0900

    response schema 유저 입력 설문데이터 추가

--- app/persistence/model/sido_codes.py
+++ app/persistence/model/sido_codes.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, BigInteger, Integer, String
 
 from app import db
+from core.domains.user.entity.user_entity import SidoCodeEntity
 
 
 class SidoCodeModel(db.Model):
@@ -13,3 +14,10 @@ class SidoCodeModel(db.Model):
     sido_name = Column(String(10), nullable=False)
     sigugun_code = Column(Integer, nullable=False)
     sigugun_name = Column(String(10), nullable=False)
+
+    def to_entity(self) -> SidoCodeEntity:
+        return SidoCodeEntity(
+            id=self.id,
+            sido_name=self.sido_name,
+            sigugun_name=self.sigugun_name,
+        )
