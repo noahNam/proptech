bf21f29b4aced4899d65cc2319f9dcd347462803
noah <noah@apartalk.com>
2021-09-15 23:39:58 +0900

    바운딩 API 리디자인

--- app/persistence/model/__init__.py
+++ app/persistence/model/__init__.py
@@ -45,3 +45,5 @@ from .dong_info_model import DongInfoModel
 from .room_info_model import RoomInfoModel
 from .room_photo_model import RoomPhotoModel
 from .notice_template_model import NoticeTemplateModel
+from .private_sale_avg_price_model import PrivateSaleAvgPriceModel
+from .public_sale_avg_price_model import PublicSaleAvgPriceModel
