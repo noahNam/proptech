17c72c5733512c6e6ed43e5fe7724203daf7fa4b
Harry <harry@apartalk.com>
2021-09-17 14:03:20 +0900

    feat: cors->IronMan 허용

--- app/extensions/__init__.py
+++ app/extensions/__init__.py
@@ -1,3 +1,4 @@
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 
 from app.extensions.cache.cache import RedisClient
@@ -6,3 +7,4 @@ from app.extensions.sens.sms import SmsClient
 jwt = JWTManager()
 sms = SmsClient()
 redis = RedisClient()
+cors = CORS()
