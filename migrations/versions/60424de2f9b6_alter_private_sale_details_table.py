15fa76b2dd8d0c0077370062d27b7ae65dd66b5d
Harry <harry@apartalk.com>
2021-09-28 11:27:52 +0900

    refactor: upsert_private_sale_avg_prices

--- migrations/versions/60424de2f9b6_alter_private_sale_details_table.py
+++ migrations/versions/60424de2f9b6_alter_private_sale_details_table.py
@@ -10,17 +10,27 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = '60424de2f9b6'
-down_revision = 'faaba582fc02'
+revision = "60424de2f9b6"
+down_revision = "faaba582fc02"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column('private_sale_details', sa.Column('contract_ym', sa.SmallInteger(), nullable=True))
-    op.create_index(op.f('ix_private_sale_details_contract_ym'), 'private_sale_details', ['contract_ym'], unique=False)
+    op.add_column(
+        "private_sale_details",
+        sa.Column("contract_ym", sa.SmallInteger(), nullable=True),
+    )
+    op.create_index(
+        op.f("ix_private_sale_details_contract_ym"),
+        "private_sale_details",
+        ["contract_ym"],
+        unique=False,
+    )
 
 
 def downgrade():
-    op.drop_index(op.f('ix_private_sale_details_contract_ym'), table_name='private_sale_details')
-    op.drop_column('private_sale_details', 'contract_ym')
+    op.drop_index(
+        op.f("ix_private_sale_details_contract_ym"), table_name="private_sale_details"
+    )
+    op.drop_column("private_sale_details", "contract_ym")
