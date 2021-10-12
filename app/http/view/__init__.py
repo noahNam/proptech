002d77851105ea6dff8c38de10826ed12ee371a0
noah <noah@apartalk.com>
2021-10-12 21:07:31 +0900

    black 적용

--- app/http/view/__init__.py
+++ app/http/view/__init__.py
@@ -31,10 +31,7 @@ def handle_custom_type_exception(error):
         return {"detail": error.code, "message": error.msg}, error.code
     else:
         return (
-            {
-                "detail": HTTPStatus.INTERNAL_SERVER_ERROR,
-                "message": str(error),
-            },
+            {"detail": HTTPStatus.INTERNAL_SERVER_ERROR, "message": str(error),},
             HTTPStatus.INTERNAL_SERVER_ERROR,
         )
 
