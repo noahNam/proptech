892882bf9a47abe1610a61fa233c27dc5e32454f
noah <noah@apartalk.com>
2021-10-26 02:13:16 +0900

    private_sale_avg_prices 컬럼 및 data type 변경

--- app/persistence/model/private_sale_avg_price_model.py
+++ app/persistence/model/private_sale_avg_price_model.py
@@ -3,7 +3,7 @@ from sqlalchemy import (
     BigInteger,
     Integer,
     DateTime,
-    SmallInteger, String,
+    String, Float,
 )
 
 from app import db
@@ -22,8 +22,9 @@ class PrivateSaleAvgPriceModel(db.Model):
         BigInteger().with_variant(Integer, "sqlite"), primary_key=True, nullable=False,
     )
     private_sales_id = Column(BigInteger, nullable=False, index=True)
-    pyoung = Column(SmallInteger, nullable=False)
-    default_pyoung = Column(SmallInteger, nullable=False)
+    pyoung = Column(Float, nullable=False)
+    default_pyoung = Column(Float, nullable=False)
+    pyoung_div = Column(String(1), nullable=False)
     trade_price = Column(Integer, nullable=True)
     deposit_price = Column(Integer, nullable=True)
     max_trade_contract_date = Column(String(8), nullable=True)
@@ -31,30 +32,16 @@ class PrivateSaleAvgPriceModel(db.Model):
     created_at = Column(DateTime, default=get_server_timestamp(), nullable=False)
     updated_at = Column(DateTime, default=get_server_timestamp(), nullable=False)
 
-    @property
-    def trade_visible(self):
-        if self.trade_price == 0:
-            return False
-        return True
-
-    @property
-    def deposit_visible(self):
-        if self.deposit_price == 0:
-            return False
-        return True
-
     def to_entity(self) -> PrivateSaleAvgPriceEntity:
         return PrivateSaleAvgPriceEntity(
             trade_info=PrivateSaleAvgPriceTradeEntity(
                 pyoung=self.pyoung,
                 trade_price=self.trade_price,
                 max_trade_contract_date=self.max_trade_contract_date,
-                trade_visible=self.trade_visible,
             ),
             deposit_info=PrivateSaleAvgPriceDepositEntity(
                 pyoung=self.pyoung,
                 deposit_price=self.deposit_price,
                 max_deposit_contract_date=self.max_deposit_contract_date,
-                deposit_visible=self.deposit_visible,
             ),
         )
