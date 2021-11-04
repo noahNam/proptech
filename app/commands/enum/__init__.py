bc914539182fa24529685ea6d076ea79fde075a8
Harry <harry@apartalk.com>
2021-11-04 17:51:45 +0900

    refactor: worker name 변경 (INSERT -> UPSERT)

--- app/commands/enum/__init__.py
+++ app/commands/enum/__init__.py
@@ -9,7 +9,7 @@ class TopicEnum(Enum):
     PRE_CALCULATE_AVERAGE_ADMINISTRATIVE = (
         "tanos.pre_calculate_average_administrative_data.v1"
     )
-    INSERT_UPLOAD_PUBLIC_SALES_AND_DETAIL_IMAGE = (
-        "tanos.insert_upload_public_sales_and_detail_image.v1"
+    UPSERT_UPLOAD_PUBLIC_SALES_AND_DETAIL_IMAGE = (
+        "tanos.upsert_upload_public_sales_and_detail_image.v1"
     )
     REPLACE_PUBLIC_TO_PRIVATE_SALES = "tanos.replace_public_to_private_sales.v1"
