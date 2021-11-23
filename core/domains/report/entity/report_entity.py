929085201ca9c176c3d52973023963d46acfb881
noah <noah@apartalk.com>
2021-11-23 17:00:08 +0900

    predicted_competitions.private_area 컬럼 추가

--- core/domains/report/entity/report_entity.py
+++ core/domains/report/entity/report_entity.py
@@ -17,6 +17,7 @@ class PredictedCompetitionEntity(BaseModel):
     id: int
     ticket_usage_result_id: int
     house_structure_type: str
+    private_area: Optional[float]
     region: str
     region_percentage: int
     multiple_children_competition: Optional[int]
---
+++ migrations/versions/7b0457032180_add_column_private_area_to_predicted_.py
@@ -0,0 +1,24 @@
+"""add column private_area to predicted_competitions table
+
+Revision ID: 7b0457032180
+Revises: c8dc095374f5
+Create Date: 2021-11-22 18:10:28.946997
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '7b0457032180'
+down_revision = 'c8dc095374f5'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('predicted_competitions', sa.Column('private_area', sa.Float(), nullable=True))
+
+
+def downgrade():
+    op.drop_column('predicted_competitions', 'private_area')
