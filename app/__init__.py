e510e68cf62fc1f6f1f56bb17ecf6b3ac1553e1e
noah <noah@apartalk.com>
2022-02-03 23:52:36 +0900

    traces_sample_rate 1.0 -> 0.01

--- app/__init__.py
+++ app/__init__.py
@@ -65,7 +65,7 @@ def init_sentry(app: Flask):
             # Set traces_sample_rate to 1.0 to capture 100%
             # of transactions for performance monitoring.
             # We recommend adjusting this value in production.
-            traces_sample_rate=1.0,
+            traces_sample_rate=0.01,
         )
 
 
