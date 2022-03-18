11abaabef0b7b10b7ebe745b78b23fc509e7157d
noah <noah@apartalk.com>
2022-03-18 14:22:04 +0900

    black 적용

--- migrations/versions/66082c511fd4_alter_survey_results_table.py
+++ migrations/versions/66082c511fd4_alter_survey_results_table.py
@@ -10,34 +10,40 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = '66082c511fd4'
-down_revision = '58192dd86224'
+revision = "66082c511fd4"
+down_revision = "58192dd86224"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column('survey_results', sa.Column('public_normal_div', sa.String(length=3), nullable=True))
-    op.add_column('survey_results', sa.Column('private_normal_div', sa.String(length=3), nullable=True))
+    op.add_column(
+        "survey_results",
+        sa.Column("public_normal_div", sa.String(length=3), nullable=True),
+    )
+    op.add_column(
+        "survey_results",
+        sa.Column("private_normal_div", sa.String(length=3), nullable=True),
+    )
     op.alter_column(
         "survey_results",
         "public_agency_recommend",
         existing_type=sa.SmallInteger(),
         type_=sa.Boolean(),
-        nullable=True
+        nullable=True,
     )
     op.alter_column(
         "survey_results",
         "private_agency_recommend",
         existing_type=sa.SmallInteger(),
         type_=sa.Boolean(),
-        nullable=True
+        nullable=True,
     )
 
 
 def downgrade():
-    op.drop_column('survey_results', 'private_normal_div')
-    op.drop_column('survey_results', 'public_normal_div')
+    op.drop_column("survey_results", "private_normal_div")
+    op.drop_column("survey_results", "public_normal_div")
     op.alter_column(
         "survey_results",
         "public_agency_recommend",
