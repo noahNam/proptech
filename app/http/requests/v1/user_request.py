3e9bd28d85ca8ca04617138ac5fc2f25ff33dedf
Harry <harry@apartalk.com>
2022-03-24 17:46:02 +0900

    hotfix: 카카오 로그인 문제로 임시 주석 처리

--- app/http/requests/v1/user_request.py
+++ app/http/requests/v1/user_request.py
@@ -43,15 +43,15 @@ class CreateUserSchema(BaseModel):
     is_auth: StrictBool
     token: StrictStr
 
-    @validator("phone_number")
-    def check_phone_number(cls, phone_number) -> Optional[str]:
-        if not phone_number:
-            return None
-
-        phone_number = "".join(phone_number.split("-"))
-        if len(phone_number) > 11:
-            raise ValidationError
-        return phone_number
+    # @validator("phone_number")
+    # def check_phone_number(cls, phone_number) -> Optional[str]:
+    #     if not phone_number:
+    #         return None
+    #
+    #     phone_number = "".join(phone_number.split("-"))
+    #     if len(phone_number) > 11:
+    #         raise ValidationError
+    #     return phone_number
 
 
 class CreateAppAgreeTermsSchema(BaseModel):
